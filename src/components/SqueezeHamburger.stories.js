b925680a5e6cbd77485707c3c9954c1b0605b821
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-01-26 16:08:18 +0100

    SqueezeHamburger

---
+++ src/components/SqueezeHamburger.stories.js
@@ -0,0 +1,8 @@
+import SqueezeHamburger from './SqueezeHamburger.vue';
+
+export default { title: 'SqueezeHamburger' };
+
+export const Default = () => ({
+  components: { SqueezeHamburger },
+  template: '<squeeze-hamburger />',
+});
